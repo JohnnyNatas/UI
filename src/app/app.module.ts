54d67278bc9810bdd41f26254d122b1967e1faa6
Marek Ziembik <Marek.Ziembik@fenergo.com>
2022-08-13 21:45:57 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HomechildComponent } from './home/homechild/homechild.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomechildComponent
   ],
   imports: [
     BrowserModule,
