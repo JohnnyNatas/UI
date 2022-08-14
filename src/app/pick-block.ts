5da31f9cbecbaecb6551ba2663633bd759f9c8ce
Marek Ziembik <Marek.Ziembik@fenergo.com>
2022-08-14 23:15:39 +0200

    Działa jak cholera !!

--- src/app/pick-block.ts
+++ src/app/pick-block.ts
@@ -2,12 +2,23 @@ export class PickBlock{
     row: number;
     column: number;
     mouseOver: boolean;
-    marked: boolean
+    _marked: boolean
+
+    get marked(){
+        if( this._marked && this.mouseOver)
+            return false
+        else 
+            return this._marked 
+    }    
+
+    set marked(value: boolean){
+        this._marked = value;        
+    }
 
     constructor(row :number,col: number){
         this.row = row;
         this.column = col;
         this.mouseOver = false;
-        this.marked = false;
+        this._marked = false;
     }
 }
